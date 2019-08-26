0bbf05f1e25b1de7b84e65f92bcb3ddccbb47c44
Wiktor Zawierucha <26853175+wik-z@users.noreply.github.com>
2019-08-26 23:22:39 +0100

    Changes html title, removes comment

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,6 @@ import './App.css';
 import AppContext from '../contexts/AppContext';
 import Router from './Router';
 
-/**
- * To-do notes
- * Threshold-only approach is not enough to ensure correct values
- * there still is a high probability it will take a completely different sound as a sample
- * Threshold + spectrum comparison is a much better approach
- */
 class App extends React.Component {
   render() {
     return (
